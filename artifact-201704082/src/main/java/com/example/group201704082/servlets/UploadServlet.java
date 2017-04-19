905e3512001e3cf92a80d0b8570afbe7d283af66
TejalSabnis <tejal.sabnis@gmail.com>
2017-04-19 14:41:51 -0700

    integrated MS Cognitive Services

--- artifact-201704082/src/main/java/com/example/group201704082/servlets/UploadServlet.java
+++ artifact-201704082/src/main/java/com/example/group201704082/servlets/UploadServlet.java
@@ -1,6 +1,7 @@
 package com.example.group201704082.servlets;
 
 import java.io.IOException;
+import java.net.URI;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
@@ -9,6 +10,15 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 
 import com.google.appengine.api.blobstore.BlobKey;
 import com.google.appengine.api.blobstore.BlobstoreService;
@@ -33,6 +43,7 @@ public class UploadServlet extends HttpServlet {
         	Entity photo = new Entity("photo", bk.getKeyString());
         	photo.setProperty("blob-key", bk.getKeyString());
         	photo.setProperty("create-time", new Date());
+        	photo.setProperty("faces", getFaces(bk.getKeyString()));
         	datastore.put(photo);
         }
         
@@ -43,5 +54,41 @@ public class UploadServlet extends HttpServlet {
         } else {
             res.sendRedirect("/serve?blob-key=" + blobKeys.get(0).getKeyString());
         }*/
+    }
+    
+    public String getFaces(String blobKey) {
+        HttpClient httpClient = new DefaultHttpClient();
+
+        try {
+            URIBuilder uriBuilder = new URIBuilder("https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize");
+
+            URI uri = uriBuilder.build();
+            HttpPost request = new HttpPost(uri);
+
+            // Request headers. Replace the example key below with your valid subscription key.
+            request.setHeader("Content-Type", "application/json");
+            request.setHeader("Ocp-Apim-Subscription-Key", "fbe1e7618e6d4fb5a51a6cc5de3fb500");
+
+            // Request body. Replace the example URL below with the URL of the image you want to analyze.
+            StringEntity reqEntity = new StringEntity("{ \"url\": \"https://project20140410.appspot.com/serve?blob-key="+blobKey+"\" }");
+            request.setEntity(reqEntity);
+
+            HttpResponse response = httpClient.execute(request);
+            HttpEntity entity = response.getEntity();
+
+            if (entity != null)
+            {
+                String faces = EntityUtils.toString(entity);
+            	System.out.println(faces);
+                return faces;
+            }
+            else {
+            	return "empty";
+            }
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+            return e.getMessage();
+        }
     }
 }
